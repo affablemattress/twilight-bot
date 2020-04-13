07a07edb3fb6eccf0e47b106c018f3d51c38a589
affablemattress <kayratopraksay@gmail.com>
2020-04-13 14:19:49 +0300

    create help.js.

---
+++ help.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const textDump = JSON.parse(fs.readFileSync("./src/secrets.json"));
+
+module.exports = {
+	main: (msgArray, msg) => {
+		main(msgArray, msg);
+	},
+};
+
+function main(msgArray, msg){
+	if (msgArray.length == 1) {
+		msg.channel.send(textDump.help.main);
+	} else {
+		switch (msgArray[1].toLowerCase()) {
+			case "weather":
+				msg.channel.send(textDump.help.lorem);
+				break;
+			default:
+				var miniHelpText = "For help, type '" + dump.commandChar + "help' or '" + dump.commandChar + "help [command]'.";
+				msg.channel.send("Help requested for unknown command [" + msgArray[1] + "]. " + miniHelpText);
+		}
+	}
+}
