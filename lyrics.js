927ca52712d09802d1c67d1451373d654af86d5e
affablemattress <kayratopraksay@gmail.com>
2020-04-15 23:55:38 +0300

    refactor.

--- lyrics.js
+++ lyrics.js
@@ -1,248 +1,254 @@
-const fs = require("fs");
-const request = require("request");
-const cheerio = require("cheerio");
+const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
 
-const dump = JSON.parse(fs.readFileSync("./src/dump.json"));
-const textDump = JSON.parse(fs.readFileSync("./src/textDump.json"));
-const secrets = JSON.parse(fs.readFileSync("./src/secrets.json"));
+const dump = JSON.parse(fs.readFileSync('./src/dump.json'));
+const textDump = JSON.parse(fs.readFileSync('./src/textDump.json'));
+const secrets = JSON.parse(fs.readFileSync('./src/secrets.json'));
 
-var miniHelpText = "For help, type '" + dump.commandChar + "help' or '" + dump.commandChar + "help [command]'.";
+const helpText = `For help, type '${dump.commandChar}help' or '${dump.commandChar}help [command]'.`;
 
-var details = {
+let details = {
 	success: false,
-	title: "",
-	artist: "",
-	thumbnailURL: "",
-	address: "",
-	search: "",
-	album: ""
-}
+	title: '',
+	artist: '',
+	thumbnailURL: '',
+	address: '',
+	search: '',
+	album: '',
+	lyrics: []
+};
 
 module.exports = {
 	main: (msgArray, msg) => {
 		main(msgArray, msg);
-	},
+	}
 };
 
-function main(msgArray, msg) {
+//WRAPPER FUNCTION
+const main = function(msgArray, msg) {
 	if (msgArray.length < 2) {
-		msg.channel.send("Invalid lyrics command. " + miniHelpText);
+		msg.channel.send(`Invalid lyrics command. ${helpText}`);
 	} else {
 		genius(msgArray, msg);
 	}
 }
 
-function getDetails(queryText, msg){
+const getDetails = function getTrackDetailsFromGeniusAPI(queryText, msg) {
 	return new Promise((resolve, reject) => {
-		request.get(secrets.api.genius.URL + "search?q=" + queryText + "&access_token=" + secrets.api.genius.key, (error, response, data) => {
+		let APIEndpoint = `${secrets.api.genius.URL}search?q=${queryText}&access_token=${secrets.api.genius.key}`;
+		request.get(APIEndpoint, (error, response, data) => {
 			if (response){
-				if(response.statusCode == 200) {
-					var GETData = JSON.parse(data);
+				if (response.statusCode == 200) {
+					let GETData = JSON.parse(data);
 					if (GETData.response.hits.length != 0) {
+						let songData = GETData.response.hits[0].result;
 						details.success = true;
-						details.title = GETData.response.hits[0].result.title;
-						details.artist = GETData.response.hits[0].result.primary_artist.name;
-						details.address = GETData.response.hits[0].result.url;
-						details.thumbnailURL = GETData.response.hits[0].result.song_art_image_thumbnail_url;
-						console.log(details)
+						details.title = songData.title;
+						details.artist = songData.primary_artist.name;
+						details.address = songData.url;
+						details.thumbnailURL = songData.song_art_image_thumbnail_url;
 						resolve();
 					} else {
 						msg.channel.send({
 							embed: {
 								color: 0x00cbb0,
 								author: {
-									name: "Twilight Bot",
+									name: 'Twilight Bot',
 									icon_url: dump.botIconURL,
 								},
-								title: "Couldn't find the lyrics for: " + details.search,
-								description: "-sad bot noises-",
+								title: `Couldn't find the lyrics for: '${details.search}'`,
+								description: '-sad bot noises-',
 								footer: {
-									text: miniHelpText
+									text: helpText
 								}
 							}
 						});
 						reject();
 					}
 				} else {
-					console.log("Genius API " + response.statusCode);
-					msg.channel.send("Genius " + textDump.down);
+					console.log(`Genius API ${response.statusCode}`);
+					msg.channel.send(`Genius ${textDump.down}`);
 					reject();
 				}
 			} else {
-				console.log("Genius API did not respond.");
-				msg.channel.send("Genius " + textDump.down);
+				console.log('Genius API did not respond.');
+				msg.channel.send(`Genius ${textDump.down}`);
 				reject();
 			}
 		});
 	});
 }
 
-function getLyrics(msg){
-	return new Promise ((resolve, reject) => {
+const getLyrics = function getTracksLyricsFromGeniusWebsite(msg) {
+	return new Promise((resolve, reject) => {
 		request.get(details.address, (error, response, data) => {
-			if(response){
+			if (response) {
 				if (response.statusCode == 200) {
-					const $ = cheerio.load(data);
-					if ($("body > routable-page .lyrics p").length != 0) {
-						details.lyrics = $("body > routable-page .lyrics p").text().split("\n").filter((value) => {
-							return value != ""
+					let $ = cheerio.load(data);
+					if ($('body > routable-page .lyrics p').length != 0) {
+						details.lyrics = $('body > routable-page .lyrics p').text().split('\n').filter((value) => {
+							return value != '';
 						});
-						resolve()
+						resolve();
 					} else {
-						msg.channel.send("Couldn't retrieve lyrics from Genius.");
-						details.success = false
+						msg.channel.send(`Couldn't retrieve lyrics from Genius.`);
+						details.success = false;
 						reject();
 					}
 				} else {
-					console.log("Genius scraper " + response.statusCode);
-					msg.channel.send("Genius " + textDump.down);
+					console.log(`Genius scraper ${response.statusCode}`);
+					msg.channel.send(`Genius ${textDump.down}`);
 					details.success = false
-					reject()
+					reject();
 				}
 			} else{
-				console.log("Genius Website did not respond.");
-				msg.channel.send("Genius " + textDump.down);
-				details.success = false
-				reject()
+				console.log('Genius Website did not respond.');
+				msg.channel.send(`Genius ${textDump.down}`);
+				details.success = false;
+				reject();
 			}
-		})
-	})
+		});
+	});
 }
 
-async function genius(msgArray, msg){
-	let queryText = "";
-	for (var i = 1; i < msgArray.length; i++) queryText += " " + msgArray[i];
+const genius = async function sendDataAcquiredFromGeniusWebsite(msgArray, msg) {
+	let queryText = '';
+	for (var i = 1; i < msgArray.length; i++) queryText += ' ' + msgArray[i];
 	queryText = queryText.trim();
-	details.search = queryText[0].toUpperCase() + queryText.slice(1);;
-	while (queryText.includes("+")) queryText = queryText.replace("+", "%2B");
+	details.search = queryText[0].toUpperCase() + queryText.slice(1);
+	while (queryText.includes('+')) queryText = queryText.replace('+', '%2B');
 	await getDetails(queryText, msg);
-	if(details.success){
+	if (details.success) {
 		await getLyrics(msg);
-		if(details.success){
-			lyrics = details.lyrics
-			var rawFields = []
-			var fieldLineCount = 0
-			var field = ["", ""]
-			for (j = 0; j < lyrics.length; j++) {
-				if (/^(\[(([a-zA-Z0-9])|\W)+\])$/.test(lyrics[j])) {
-					if (field[0] == "" && field[1] == "") {
-						field[0] = lyrics[j];
-					} else if (field[0] != "" && field[1] == "") {
-						field[1] = "\u200b";
+		if (details.success) {
+			let lyrics = details.lyrics;
+			let rawFields = [];
+			let fieldLineCount = 0;
+			let field = ['', ''];
+			lyrics.forEach((line) => {
+				if (/^(\[(([a-zA-Z0-9])|\W)+\])$/.test(line)) {
+					if (field[0] == '' && field[1] == '') {
+						field[0] = line;
+					} else if (field[0] != '' && field[1] == '') {
+						field[1] = '\u200b';
+						rawFields.push({
+							name: field[0],
+							value: field[1]
+						});
+						field = [line, ''];
+						fieldLineCount = 0;
+					} else if (field[0] == '' && field[1] != '') {
+						field[0] = '\u200b';
+						rawFields.push({
+							name: field[0],
+							value: field[1]
+						});
+						field = [line, ''];
+						fieldLineCount = 0;
+					} else {
 						rawFields.push({
 							name: field[0],
 							value: field[1]
 						})
-						field = [lyrics[j], ""]
-						fieldLineCount = 0
-					} else if (field[0] == "" && field[1] != "") {
-						field[0] = "\u200b";
-						rawFields.push({
-							name: field[0],
-							value: field[1]
-						})
-						field = [lyrics[j], ""]
-						fieldLineCount = 0
-					} else{
-						rawFields.push({
-							name: field[0],
-							value: field[1]
-						})
-						field = [lyrics[j], ""]
-						fieldLineCount = 0
+						field = [line, ''];
+						fieldLineCount = 0;
 					}
-				} else{
+				} else {
 					if (field[1].length < 900) {
-						field[1] += lyrics[j] + "\n"
+						field[1] += line + '\n';
 						fieldLineCount++;
-					} else{
-						if (field[0] == ""){
-							field[0] = "\u200b";
+					} else {
+						if (field[0] == '') {
+							field[0] = '\u200b';
 							rawFields.push({
 								name: field[0],
 								value: field[1]
-							})
-							field = ["", lyrics[j]]
-							fieldLineCount = 0
-						} else{
+							});
+							field = ['', line];
+							fieldLineCount = 0;
+						} else {
 							rawFields.push({
 								name: field[0],
 								value: field[1]
-							})
-							field = ["", lyrics[j]]
-							fieldLineCount = 0
+							});
+							field = ['', line];
+							fieldLineCount = 0;
 						}
 					}
-				} 
+				}
+			})
+
+			if (field[0] == '' && field[1] != '') {
+				field[0] = '\u200b';
+				rawFields.push({
+					name: field[0],
+					value: field[1]
+				});
+			} else if (field[0] != '' && field[1] == '') {
+				field[1] = '\u200b';
+				rawFields.push({
+					name: field[0],
+					value: field[1]
+				});
+			} else if (field[0] != '' && field[1] != '') {
+				rawFields.push({
+					name: field[0],
+					value: field[1]
+				});
 			}
-			if (field[0] == "" && field[1] != "") {
-				field[0] = "\u200b";
-				rawFields.push({
-					name: field[0],
-					value: field[1]
-				})
-			} else if (field[0] != "" && field[1] == "") {
-				field[1] = "\u200b";
-				rawFields.push({
-					name: field[0],
-					value: field[1]
-				})
-			} else if (field[0] != "" && field[1] != "") {
-				rawFields.push({
-					name: field[0],
-					value: field[1]
-				})
-			}
-			var rawEmbeds = []
-			var rawEmbedFields= []
-			for(n=0; n < rawFields.length; n++){
-				if(rawEmbedFields.length <5){
-					rawEmbedFields.push(rawFields[n]);
+
+			let rawEmbeds = [];
+			let rawEmbedFields= [];
+			rawFields.forEach((rawField) => {
+				if (rawEmbedFields.length < 5) {
+					rawEmbedFields.push(rawField);
 				} else {
 					rawEmbeds.push(rawEmbedFields);
 					rawEmbedFields = [];
 				}
-			}
+			});
+
 			if(rawEmbedFields.length){
-				rawEmbeds.push(rawEmbedFields)
+				rawEmbeds.push(rawEmbedFields);
 			}
-			for(k = 0; k < rawEmbeds.length; k++) {
-				rawEmbed = {
+
+			rawEmbeds.forEach((rawEmbed, index) => {
+				embed = {
 					color: 0x00cbb0,
-					fields: rawEmbeds[k],
-				}
-				if (k == 0) {
+					fields: rawEmbed,
+				};
+				if (index == 0) {
 					embed = {
-						...rawEmbed,
+						...embed,
 						...{
 							author: {
-								name: "Twilight Bot",
+								name: 'Twilight Bot',
 								icon_url: dump.botIconURL,
 							},
-							title: "Lyrics for '" + details.title + "'",
-							description: "By "  + details.artist,
+							title: `Lyrics for '${details.title}'`,
+							description: `By ${details.artist}`,
 							thumbnail: {
 								url: details.thumbnailURL,
 							},
 						}
-					}
-				} else if (k == rawEmbeds.length - 1) {
+					};
+				} else if (index == rawEmbeds.length - 1) {
 					embed = {
-						...rawEmbed,
+						...embed,
 						...{
 							footer: {
 								text: 'Lyrics acquired from Genius',
 								icon_url: secrets.api.genius.iconURL
 							}
 						}
-					}
-				} else {
-					embed = rawEmbed
+					};
 				}
+
 				msg.channel.send({
 					embed: embed
-				})
-			}
+				});
+			});
 		}
 	}
 }
