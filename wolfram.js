eb41d9e40701df0b095aa182a264dcf5052c8d71
affablemattress <kayratopraksay@gmail.com>
2020-04-16 00:06:54 +0300

    refactor.

--- wolfram.js
+++ wolfram.js
@@ -1,11 +1,11 @@
-const fs = require("fs");
-const request = require("request");
+const fs = require('fs');
+const request = require('request');
 
-const dump = JSON.parse(fs.readFileSync("./src/dump.json"));
-const textDump = JSON.parse(fs.readFileSync("./src/textDump.json"));
-const secrets = JSON.parse(fs.readFileSync("./src/secrets.json"));
+const dump = JSON.parse(fs.readFileSync('./src/dump.json'));
+const textDump = JSON.parse(fs.readFileSync('./src/textDump.json'));
+const secrets = JSON.parse(fs.readFileSync('./src/secrets.json'));
 
-const miniHelpText = "For help, type '" + dump.commandChar + "help' or '" + dump.commandChar + "help [command]'.";
+const helpText = `For help, type '${dump.commandChar}help' or '${dump.commandChar}help [command]'.`;
 
 module.exports = {
 	main: (msgArray, msg) => {
@@ -16,28 +16,28 @@ module.exports = {
 //WRAPPER FUNCTION
 function main(msgArray, msg){
 	if (msgArray.length < 2) {
-		msg.channel.send("Invalid wolfram command. " + miniHelpText);
+		msg.channel.send(`Invalid wolfram command. ${helpText}`);
 	} else {
 		wolfram(msgArray, msg);
 	}
 }
 
-//Input-> data: Hex data acquired from WolframAlpha Simple Answer API, queryText: URL encoded query which was sent to API. Return-> None.
-function wolfram(msgArray, msg){
-	let queryText = "";
-	for (var i = 1; i < msgArray.length; i++) queryText += " " + msgArray[i];
+const wolfram = function sendDataAcquiredFromWolframAlphaShortAnswerAPI(msgArray, msg) {
+	let queryText = '';
+	for (var i = 1; i < msgArray.length; i++) queryText += ' ' + msgArray[i];
 	let search = queryText.trim();
-	while (queryText.includes("+")) queryText = queryText.replace("+", "%2B");
-	request.get(secrets.api.wolframAlpha.URL + "result?i=" + queryText + "&units=metric&appid=" + secrets.api.wolframAlpha.key, (error, response, data) => {
-		if(response){	
+	while (queryText.includes('+')) queryText = queryText.replace('+', '%2B');
+	let APIEndpoint = `${secrets.api.wolframAlpha.URL}result?i=${queryText}&units=metric&appid=${secrets.api.wolframAlpha.key}`;
+	request.get(APIEndpoint, (error, response, data) => {
+		if (response) {	
 			if (response.statusCode == 200 || response.statusCode == 501) {
-				search = "Answer to Query: " + search[0].toUpperCase() + search.slice(1);
+				search = 'Answer to Query: ' + search[0].toUpperCase() + search.slice(1);
 				data = data.toString()[0].toUpperCase() + data.toString().slice(1);
 				msg.channel.send({
 					embed: {
 						color: 0x00cbb0,
 						author: {
-							name: "Twilight Bot",
+							name: 'Twilight Bot',
 							icon_url: dump.botIconURL,
 						},
 						title: search,
@@ -49,12 +49,12 @@ function wolfram(msgArray, msg){
 					}
 				});
 			} else {
-				console.log("Wolfram " + response.statusCode);
-				msg.channel.send("WolframAlpha " + textDump.down);
+				console.log('Wolfram ' + response.statusCode);
+				msg.channel.send('WolframAlpha ' + textDump.down);
 			}
 		} else{
-			console.log("Wolfram API did not respond.");
-			msg.channel.send("WolframAlpha " + textDump.down);
+			console.log('Wolfram API did not respond.');
+			msg.channel.send('WolframAlpha ' + textDump.down);
 		}
-	})
+	});
 }
