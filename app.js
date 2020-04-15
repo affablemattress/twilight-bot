b4e38d243557f7bd7520c7a37233c86f292b75bf
affablemattress <kayratopraksay@gmail.com>
2020-04-16 00:51:11 +0300

    refactor.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const weather = require('./weather.js');
-const wolfram = require('./wolfram.js')
-const dice = require('./dice.js');
 const help = require('./help.js');
-const lyrics = require('./lyrics.js')
+const dice = require('./dice.js');
+const lyrics = require('./lyrics.js');
+const wolfram = require('./wolfram.js');
+const weather = require('./weather.js');
 
 const secrets = JSON.parse(fs.readFileSync('./src/secrets.json'));
 const dump = JSON.parse(fs.readFileSync('./src/dump.json'));
@@ -27,33 +27,39 @@ client.on('message', msg => {
 		switch (msgArray[0].toLowerCase()){
 
 //Help. Send text from 'textDump.json'.
-			case 'help':
+			case 'help': {
 				help.main(msgArray, msg);
 				break;
+			}
 
 //Weather.Send data from Open Weather Map API as embed.
-			case 'weather':
+			case 'weather': {
 				weather.main(msgArray, msg);
 				break;
+			}
 
 //Wolfram. Send a simple text answer from WolframAlpha as embed.
-			case 'wolfram':
+			case 'wolfram': {
 				wolfram.main(msgArray, msg);
 				break;
+			}
 
 //Dice. Send throw results as embed.
-			case 'dice':
+			case 'dice': {
 				dice.main(msgArray, msg);
 				break;
+			}
 
 //Lyrics. Send lyrics scraped from Genius.
-			case 'lyrics':
+			case 'lyrics': {
 				lyrics.main(msgArray, msg);
 				break;
+			}
 
 //EOTL. Unknown command.
-			default:
+			default: {
 				msg.channel.send(helpText);
+			}
 		}
 	}
 });
--- dice.js
+++ dice.js
@@ -1,7 +1,8 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const dump = JSON.parse(fs.readFileSync("./src/dump.json"));
+const dump = JSON.parse(fs.readFileSync('./src/dump.json'));
 
+const helpText = `For help, type '${dump.commandChar}help' or '${dump.commandChar}help [command]'.`;
 
 module.exports = {
 	main: (msgArray, msg) => {
@@ -19,136 +20,158 @@ function main(msgArray, msg){
 	}
 }
 
-//Input-> msg: Discord.js msg object. Return-> None.
-//Throws a die. Sends the result as embed.
-function die(msg){
-	var roll = Math.ceil(Math.random()*6).toString();
-	var rollText = roll == 6 ? {footer:{text: "Rolled a " + roll + ". Nice one!"}}: {footer:{text: "Rolled a " + roll + "."}};
-	var rollText = roll == 1 ? {footer:{text: "Rolled a " + roll + ". Nice 1!"}}: rollText;
-	msg.channel.send({embed: {...{
-			color: 0x00cbb0,
-			author: {
-				name: "Twilight Bot",
-				icon_url: dump.botIconURL,
-			},
-			title: "Rolling for " + msg.author.username + "...",
-			image: {
-				url: dump.diceURL[roll],
-			},
-		}, ...rollText}
+const die = function rollDieThenSendResult(msg) {
+	let roll = Math.ceil(Math.random() * 6).toString();
+	let rollText = "";
+	rollText = roll == 6 
+		? {
+			footer: { text: `Rolled a ${roll} Nice one!` }
+		} 
+		: {
+			footer: { text: `Rolled a ${roll}.` }
+		};
+	rollText = roll == 1 
+		? {
+			footer: { text: 'Rolled a ' + roll + '. Nice 1!' }
+		}
+		: rollText;
+	msg.channel.send({
+		embed: {
+			...{
+				color: 0x00cbb0,
+				author: {
+					name: 'Twilight Bot',
+					icon_url: dump.botIconURL,
+				},
+				title: 'Rolling for ' + msg.author.username + '...',
+				image: {
+					url: dump.diceURL[roll],
+				},
+			}, 
+			...rollText
+		}
 	});
 }
 
-//Input-> queryInput: user input - command, msg: Discord.js msg object. Return-> None.
-//First checks each element in the array if they are a valid input via Regex. Then throws the dice accordingly. After that applies the modifiers. And finally sends the data as Discord.js embed object.
-function dice(queryInput, msg){
-	var miniHelpText = "For help, type '" + dump.commandChar + "help' or '" + dump.commandChar + "help [command]'.";
-	var HALEmbed = {
+const dice = function validateInputRollDiceApplyModifiersSendResult(queryInput, msg) {
+	let HALEmbed = {
 		color: 0x00cbb0,
 		author: {
-			name: "HAL 9000",
+			name: 'HAL 9000',
 			icon_url: dump.HALIconURL,
 		},
-		title: "I'm sorry " + msg.author.username + ", I'm afraid I can't do that.",
-		description: "I think you know what the problem is just as well as I do.",
+		title: `I'm sorry ${msg.author.username}, I'm afraid I can't do that.`,
+		description: 'I think you know what the problem is just as well as I do.',
 		footer: {
-			text: miniHelpText
+			text: helpText
 		}
 	}
-	if(queryInput.length > 10){
+	if (queryInput.length > 10) {
 		msg.channel.send({
 			embed: HALEmbed
 		});
-	} else{
-		var uniqueQueries = []; var queries = {}; var sumQueries= [];
-		for(var i = 0; i < queryInput.length; i++){
-			var query = []; 
-			if(/^(\-?[0-9]{0,3}d[1-9][0-9]{0,2})$/.test(queryInput[i].replace(/^\++/g, ""))){
-				query = queryInput[i].replace(/^\++/g, "").split("d");
-				if(query[0][0] == "-"){
-					if(uniqueQueries.includes("-" + query[1])){
-						queries["-" + query[1]] = queries[query[1]] + parseInt(query[0].slice(1));
-					} else{
-						uniqueQueries.push("-" + query[1]);
-						queries["-" + query[1]] = parseInt(query[0].slice(1));
+	} else {
+		let uniqueQueries = [];
+		let queries = {};
+		let sumQueries= [];
+
+		queryInput.forEach((rawQuery) => {
+			let query = [];
+			if (/^(\-?[0-9]{0,3}d[1-9][0-9]{0,2})$/.test(rawQuery.replace(/^\++/g, ''))) {
+				query = rawQuery.replace(/^\++/g, '').split('d');
+				if (query[0][0] == '-') {
+					if (uniqueQueries.includes('-' + query[1])) {
+						queries['-' + query[1]] = queries[query[1]] + parseInt(query[0].slice(1));
+					} else {
+						uniqueQueries.push('-' + query[1]);
+						queries['-' + query[1]] = parseInt(query[0].slice(1));
 					}
-				} else{
-					if(uniqueQueries.includes(query[1])){
+				} else {
+					if (uniqueQueries.includes(query[1])) {
 						queries[query[1]] = queries[query[1]] + parseInt(query[0]);
-					} else{
+					} else {
 						uniqueQueries.push(query[1]);
 						queries[query[1]] = parseInt(query[0]);
 					}
 				}
-			} else if(/^((\+|-)[0-9]{1,3})$/.test(queryInput[i])){
-				sumQueries.push(" " + queryInput[i]);
+			} else if (/^((\+|-)[0-9]{1,3})$/.test(rawQuery)) {
+				sumQueries.push(' ' + rawQuery);
 			} else {
 				msg.channel.send({
 					embed: HALEmbed
 				});
 			}
-		}
-		if(uniqueQueries.length == 0){
+		});
+
+		if (uniqueQueries.length == 0) {
 			msg.channel.send({
 				embed: HALEmbed
 			});
 		}
+
+		let fields = [];
+		let sum = 0; 
+		let numberOfDice = 0;
+
 		uniqueQueries = uniqueQueries.sort((a, b) => a - b);
-		var fields = []; var sum = 0; var numberOfDice = 0;
-		for(var i = 0; i<uniqueQueries.length; i++){
-			if(queries[uniqueQueries[i]]>100){
+		uniqueQueries.forEach((uniqueQuery) => {
+			if (queries[uniqueQuery] > 100) {
 				msg.channel.send({
 					embed: HALEmbed
 				});
 			}
-		}
-		for(var i = 0; i<uniqueQueries.length; i++){
-			numberOfDice += parseInt(queries[uniqueQueries[i].toString()]);
-			var roll = 0;
-			var rollArray = [];
-			var uniqueSum = 0;
-			for(var k = 0; k<queries[uniqueQueries[i].toString()]; k++){
-				roll = Math.ceil(Math.random() * uniqueQueries[i]);
-				if(uniqueQueries[i] < 0){
-					roll --;
+		});
+
+		uniqueQueries.forEach((uniqueQuery) => {
+			numberOfDice += parseInt(queries[uniqueQuery.toString()]);
+			let roll = 0;
+			let rollArray = [];
+			let uniqueSum = 0;
+			for (let i = 0; i < queries[uniqueQuery.toString()]; i++) {
+				roll = Math.ceil(Math.random() * uniqueQuery);
+				if (uniqueQuery < 0) {
+					roll--;
 				}
 				uniqueSum += roll;
 				sum += roll;
-				rollArray.push(" " + roll.toString());
+				rollArray.push(' ' + roll.toString());
 			}
+
 			fields.push({
-				name: queries[uniqueQueries[i].toString()] + "d" + uniqueQueries[i].toString() + ". Total: " + uniqueSum,
+				name: `${queries[uniqueQuery.toString()]}d${uniqueQuery.toString()}. Total: ${uniqueSum}`,
 				value: rollArray.toString(),
 				inline: false
 			});
-		}
-		var sumQueriesSum = 0;
-		for(var i = 0; i < sumQueries.length; i++){
-			sumQueriesSum += parseInt(sumQueries[i]);
-		}
-		sum += sumQueriesSum;
-		if(sumQueries.length > 0){
+		});
+
+		if (sumQueries.length > 0) {
+			let sumQueriesSum = 0;
+			sumQueries.forEach((sumQuery) => {
+				sumQueriesSum += parseInt(sumQuery)
+			});
+			sum += sumQueriesSum;
 			fields.push({
-				name: "Modifiers. Total: " + sumQueriesSum,
+				name: `Modifiers. Total: ${sumQueriesSum}`,
 				value: sumQueries.toString(),
 				inline: false
 			});
 		}
+
 		msg.channel.send({
 			embed: {
 				color: 0x00cbb0,
 				author: {
-					name: "Twilight Bot",
+					name: 'Twilight Bot',
 					icon_url: dump.botIconURL,
 				},
-				title: "Rolled for " + msg.author.username + ".",
-				description: "Rolled " + numberOfDice + " dice. Total: " + sum,
+				title: `Rolled for ${msg.author.username}.`,
+				description: `Rolled ${numberOfDice} dice. Total: ${sum}`,
 				fields: fields,
 				thumbnail: {
 					url: dump.d20IconURL,
 				},
 				footer: {
-					text: miniHelpText
+					text: helpText
 				}
 			}
 		});
