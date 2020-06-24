133807054d2b1fe8bbc6d943655f9104efd5eadf
affablemattress <kayratopraksay@gmail.com>
2020-06-24 22:50:21 +0300

    add prefix command.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const help = require('./commands/help.js');
+
 const dice = require('./commands/dice.js');
+const help = require('./commands/help.js');
 const lyrics = require('./commands/lyrics.js');
-const wolfram = require('./commands/wolfram.js');
+const prefixJS = require('./commands/prefix.js');
 const weather = require('./commands/weather.js');
+const wolfram = require('./commands/wolfram.js');
 
 const secrets = JSON.parse(fs.readFileSync('./src/secrets.json'));
-const dump = JSON.parse(fs.readFileSync('./src/dump.json'));
+let prefixStorage = JSON.parse(fs.readFileSync('./src/prefixStorage.json'));
 
+let prefix = prefixStorage.defaultPrefix;
 const client = new Discord.Client();
-const commandChar = dump.commandChar;
-
-const helpText = `Unknown command. For help, type '${commandChar}help' or '${commandChar}help [command]'.`;
 
 client.on('ready', () => {
 		console.log('Twilight Bot is ready to roll!');
@@ -21,46 +21,61 @@ client.on('ready', () => {
 
 //Get user input.
 client.on('message', msg => {
-	if (msg.content.trim()[0] === commandChar){
+	if (!(msg.guild.id in prefixStorage.prefixes)) {
+		prefix = prefixStorage.defaultPrefix;
+	} else {
+		prefix = prefixStorage.prefixes[msg.guild.id];
+	}
+
+	if (msg.content.trim()[0] === prefix) {
 		let msgArray =  msg.content.trim().slice(1).split(' ');
 		console.log(msgArray);
 		switch (msgArray[0].toLowerCase()){
 
-//Help. Send text from 'textDump.json'.
-			case 'help': {
-				help.main(msgArray, msg);
-				break;
-			}
-
-//Weather.Send data from Open Weather Map API as embed.
-			case 'weather': {
-				weather.main(msgArray, msg);
-				break;
-			}
-
-//Wolfram. Send a simple text answer from WolframAlpha as embed.
-			case 'wolfram': {
-				wolfram.main(msgArray, msg);
-				break;
-			}
-
 //Dice. Send throw results as embed.
 			case 'dice': {
-				dice.main(msgArray, msg);
+				dice.main(msgArray, msg, prefix);
+				break;
+			}
+
+//Help. Send help to degenerates.'.
+			case 'help': {
+				help.main(msgArray, msg, prefix);
 				break;
 			}
 
 //Lyrics. Send lyrics scraped from Genius.
 			case 'lyrics': {
-				lyrics.main(msgArray, msg);
+				lyrics.main(msgArray, msg, prefix);
+				break;
+			}
+
+//Prefix. Change bot's command prefix.
+			case 'prefix': {
+				prefixJS.main(msgArray, msg, prefix);
+				prefixStorage = JSON.parse(fs.readFileSync('./src/prefixStorage.json'));
+				break;
+			}
+
+//Weather.Send data from Open Weather Map API as embed.
+			case 'weather': {
+				weather.main(msgArray, msg, prefix);
+				break;
+			}
+
+//Wolfram. Send a simple text answer from WolframAlpha as embed.
+			case 'wolfram': {
+				wolfram.main(msgArray, msg, prefix);
 				break;
 			}
 
 //EOTL. Unknown command.
 			default: {
-				msg.channel.send(helpText);
+				msg.channel.send(`Unknown command. For help, type '${prefix}help' or '${prefix}help [command]'.`);
 			}
 		}
+	} else if (msg.content == '$prefix') {
+		prefixJS.main(['prefix'], msg);
 	}
 });
 
---
+++ commands/prefix.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+const storage = JSON.parse(fs.readFileSync(__dirname + '/../src/storage.json'));
+let prefixStorage = JSON.parse(fs.readFileSync(__dirname + '/../src/prefixStorage.json'));
+
+//WRAPPER FUNCTION
+const main = async function (msgArray, msg, prefix) {
+	if (msgArray.length == 1) {
+		showPrefix(msg, prefix);
+	} else {
+		editPrefix(msgArray, msg);
+	}
+}
+
+module.exports.main = main;
+
+const showPrefix = function sendGuildPrefix(msg, prefix) {
+	msg.channel.send({
+		embed: {
+			color: 0x00cbb0,
+			author: {
+				name: 'Twilight Bot',
+				icon_url: storage.botIconURL,
+			},
+			title: `Prefix for ${msg.guild.name} is '${prefix}'`,
+			description: `For help, type '${prefix}help' or '${prefix}help [command]'.`
+		},
+	});
+}
+
+const editPrefix = function editGuildPrefixAtPrefixStorage(msgArray, msg) {
+	if (/[^a-zA-Z\d\s:@]/.test(msgArray[1].slice(0, 1))) {
+		prefixStorage = JSON.parse(fs.readFileSync(__dirname + '/../src/prefixStorage.json'));
+		prefixStorage.prefixes[msg.guild.id] = msgArray[1].slice(0, 1);
+		fs.writeFileSync(__dirname + '/../src/prefixStorage.json', JSON.stringify(prefixStorage));
+		msg.channel.send({
+			embed: {
+				color: 0x00cbb0,
+				author: {
+					name: 'Twilight Bot',
+					icon_url: storage.botIconURL,
+				},
+				title: `Prefix for ${msg.guild.name} is changed to '${msgArray[1].slice(0, 1)}'`,
+				description: `For help, type '${msgArray[1].slice(0, 1)}help' or '${msgArray[1].slice(0, 1)}help [command]'.`
+			},
+		});
+	} else {
+		msg.channel.send(`'${msgArray[1].slice(0, 1)}' is not a valid prefix. Try a non-alphanumeric character except '@'.`);
+	}
+}
